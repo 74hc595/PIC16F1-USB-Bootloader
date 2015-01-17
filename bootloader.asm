38cc6ae01fbede287106c18c8a4315f6cb1c48fd
Matt Sarnoff <matt.sarnoff@gmail.com>
2015-01-16 21:48:32 -0800

    moved logging to a separate file

--- bootloader.asm
+++ bootloader.asm
@@ -6,6 +6,7 @@
 ; FSR0L, FSR0H, FSR1L, and FSR1H are used to pass additional arguments
 ; to functions, and may be used as scratch registers inside of functions.
 
+LOGGING_ENABLED	equ	1
 
 	radix dec
 	list n=0,st=off
@@ -197,6 +198,17 @@ _wait_osc_ready
 ; Print a power-on character
 	call	log_init
 	logch	'^',LOG_NEWLINE
+	mloghex	8,0
+	mlogl	0x01
+	mlogl	0x23
+	mlogl	0x45
+	mlogl	0x67
+	mlogl	0x89
+	mlogl	0xAB
+	mlogl	0xCD
+	mlogl	0xEF
+	mlogch	'!',LOG_NEWLINE
+
 
 ; Initialize USB
 	;call	usb_init
@@ -661,171 +673,7 @@ _bcdone	movfw	FSR0L
 	movwf	EP0_DATA_IN_PTRH
 	return
 
-
-;;;;;;; Low-latency logging
-
-; Linear address of the 256-byte buffer
-; Aligned to a 256-byte boundary
-LOG_BUFFER	equ	0x2300
-
-; Banked RAM locations used by logging system
-; (directly before the log buffer)
-LOG_WREG_SAVE	equ	0x539		; bank 0x0A
-LOG_FSR0L_SAVE	equ	0x53a
-LOG_FSR0H_SAVE	equ	0x53b
-LOG_HEAD	equ	0x53c
-LOG_TAIL	equ	0x53d
-LOG_FMT_FLAGS	equ	0x53e		; hex count/newline/space flags
-LOG_CURR_BYTE	equ	0x53f
-
-log_init
-	banksel	LOG_HEAD
-	clrf	LOG_HEAD
-	clrf	LOG_TAIL
-	clrf	LOG_FMT_FLAGS
-	return
-
-log_service
-	banksel	LOG_HEAD
-_lsloop	movfw	LOG_TAIL	; if head == tail, buffer is empty
-	subwf	LOG_HEAD,w
-	skpnz
-	return
-;	banksel	LATA
-;	movlw	(1<<LATA5)
-;	xorwf	LATA,f
-;	banksel	LOG_HEAD
-; dequeue a byte
-	movlw	LOG_BUFFER>>8
-	movwf	FSR0H
-	movfw	LOG_HEAD
-	movwf	FSR0L
-	moviw	FSR0++
-	movwf	LOG_CURR_BYTE
-; save advanced head pointer
-	movfw	FSR0L
-	movwf	LOG_HEAD
-; are we in hex mode?
-	btfsc	LOG_FMT_FLAGS,7
-	goto	_lsphex
-; process the byte
-	btfsc	LOG_CURR_BYTE,7	; is this a hex marker?
-	goto	_lsshex
-; we're just in ASCII mode
-	movfw	LOG_CURR_BYTE
-	andlw	b'00111111'	; mask off high bits
-	addlw	32		; upper bits need to be set properly
-	call	_uart_print_ch
-	btfsc	LOG_CURR_BYTE,6	; need to print a trailing newline?
-	call	_uart_print_nl
-	goto	_lsloop
-_lsshex	movfw	LOG_CURR_BYTE	; starting hex mode? write flags and loop
-	movwf	LOG_FMT_FLAGS
-	goto	_lsloop	
-_lsphex	btfsc	LOG_FMT_FLAGS,5	; need to print a leading space?
-	call	_uart_print_space
-	call	_uart_print_hex	; prints byte in LOG_CURR_BYTE
-	movfw	LOG_FMT_FLAGS	; decrement hex byte count
-	andlw	b'00011111'	; isolate count bits
-	decfsz	WREG,w		; subtract 1
-	goto	_nxthex
-; hex counter reached 0; print newline if needed and clear format flags
-	btfsc	LOG_FMT_FLAGS,6	; need to print a trailing newline?
-	call	_uart_print_nl
-	clrf	LOG_FMT_FLAGS
-	goto	_lsloop
-; hex counter > 0; write back new count
-_nxthex	xorwf	LOG_FMT_FLAGS,w	; xor-swap new count and old flags
-	xorwf	LOG_FMT_FLAGS,f
-	xorwf	LOG_FMT_FLAGS,w	; LOG_FMT_FLAGS contains new count, but NOT old flags
-	andlw	b'11100000'	; isolate old flags
-	iorwf	LOG_FMT_FLAGS,f	; write them back to LOG_FMT_FLAGS
-	goto	_lsloop
-
-
-
-log_single_byte
-	banksel	LOG_WREG_SAVE	; need to save W and FSR0
-	movwf	LOG_WREG_SAVE
-	movfw	FSR0L
-	movwf	LOG_FSR0L_SAVE
-	movfw	FSR0H
-	movwf	LOG_FSR0H_SAVE
-	call	_log_byte_inner	; and fall through
-log_multi_byte_end
-	banksel	LOG_FSR0L_SAVE	; (redundant when falling through)
-; restore registers
-	movfw	LOG_FSR0L_SAVE
-	movwf	FSR0L
-	movfw	LOG_FSR0H_SAVE
-	movwf	FSR0H
-	movfw	LOG_WREG_SAVE
-	return
-
-log_multi_byte_start
-; save FSR0
-	banksel	LOG_WREG_SAVE
-	movfw	FSR0L
-	movwf	LOG_FSR0L_SAVE
-	movfw	FSR0H
-	movwf	LOG_FSR0H_SAVE
-	return
-
-log_byte
-	banksel	LOG_WREG_SAVE
-	movwf	LOG_WREG_SAVE	;and fall through
-; load tail pointer into FSR0
-_log_byte_inner
-	movlw	LOG_BUFFER>>8
-	movwf	FSR0H
-	movfw	LOG_TAIL
-	movwf	FSR0L
-	movfw	LOG_WREG_SAVE	; write byte into log buffer
-	movwi	FSR0++		; advance tail pointer
-; save new tail pointer
-	movfw	FSR0L		; high byte is ignored for 256-byte wraparound
-	movwf	LOG_TAIL
-; always keep one slot open; if (tail+1)%256 == head, advance head
-	subwf	LOG_HEAD,w
-	skpnz
-	incf	LOG_HEAD,f
-	return
-
-
-_uart_print_space
-	movlw	' '
-	goto	_uart_print_ch
-_uart_print_nl
-	movlw	'\n'
-; prints char in W
-_uart_print_ch
-	banksel	TXREG
-	movwf	TXREG		; transmit the character
-	banksel	PIR1		; need 1 cycle delay before checking TXIF
-	waitfs	PIR1,TXIF	; loop until character is sent
-	banksel	LOG_FMT_FLAGS	; preserve BSR
-	return
-
-
-
-;;; Converts the lower nibble of W to its ASCII hexadecimal representation.
-_w2hd	macro			; 'w to hex digit'
-	andlw	b'00001111'
-	subwl	10		; is W >= 10?
-	skpnc
-	addlw	7		; if so, shift to letters
-	addlw	'A'-7		; shift to printable ASCII
-	endm
-_uart_print_hex
-	movfw	LOG_CURR_BYTE
-	swapf	WREG,w		; get high nibble
-	_w2hd
-	call	_uart_print_ch	; print high nibble
-	movfw	LOG_CURR_BYTE	; bring back original byte
-	_w2hd
-	goto	_uart_print_ch	; print low nibble
-
-
+	include "log.asm"
 
 	if 0
 ;;; Transmits a newline over the UART.
---
+++ log.asm
@@ -0,0 +1,173 @@
+; vim:noet:sw=8:ts=8:ai:syn=pic
+;
+; Minimal, low-latency UART logging system.
+; Uses a 256-byte circular buffer aligned to a 256-byte boundary.
+; By default, this is placed at the end of RAM.
+;
+; To write to the log, use the macros in log_macros.inc.
+; To print the contents of the log over the UART, call log_service
+; periodically from your main loop.
+; The write macros are safe to use in an interrupt handler.
+
+; Linear address of the 256-byte buffer.
+; Must be aligned to a 256-byte boundary.
+LOG_BUFFER	equ	0x2300
+
+; Banked RAM locations used by logging system
+; (directly before the log buffer)
+LOG_WREG_SAVE	equ	0x539		; bank 0x0A
+LOG_FSR0L_SAVE	equ	0x53a
+LOG_FSR0H_SAVE	equ	0x53b
+LOG_HEAD	equ	0x53c
+LOG_TAIL	equ	0x53d
+LOG_FMT_FLAGS	equ	0x53e		; hex count/newline/space flags
+LOG_CURR_BYTE	equ	0x53f
+
+log_init
+	banksel	LOG_HEAD
+	clrf	LOG_HEAD
+	clrf	LOG_TAIL
+	clrf	LOG_FMT_FLAGS
+	return
+
+log_service
+	banksel	LOG_HEAD
+_lsloop	movfw	LOG_TAIL	; if head == tail, buffer is empty
+	subwf	LOG_HEAD,w
+	skpnz
+	return
+;	banksel	LATA
+;	movlw	(1<<LATA5)
+;	xorwf	LATA,f
+;	banksel	LOG_HEAD
+; dequeue a byte
+	movlw	LOG_BUFFER>>8
+	movwf	FSR0H
+	movfw	LOG_HEAD
+	movwf	FSR0L
+	moviw	FSR0++
+	movwf	LOG_CURR_BYTE
+; save advanced head pointer
+	movfw	FSR0L
+	movwf	LOG_HEAD
+; are we in hex mode?
+	btfsc	LOG_FMT_FLAGS,7
+	goto	_lsphex
+; process the byte
+	btfsc	LOG_CURR_BYTE,7	; is this a hex marker?
+	goto	_lsshex
+; we're just in ASCII mode
+	movfw	LOG_CURR_BYTE
+	andlw	b'00111111'	; mask off high bits
+	addlw	32		; upper bits need to be set properly
+	call	_uart_print_ch
+	btfsc	LOG_CURR_BYTE,6	; need to print a trailing newline?
+	call	_uart_print_nl
+	goto	_lsloop
+_lsshex	movfw	LOG_CURR_BYTE	; starting hex mode? write flags and loop
+	movwf	LOG_FMT_FLAGS
+	goto	_lsloop	
+_lsphex	btfsc	LOG_FMT_FLAGS,5	; need to print a leading space?
+	call	_uart_print_space
+	call	_uart_print_hex	; prints byte in LOG_CURR_BYTE
+	movfw	LOG_FMT_FLAGS	; decrement hex byte count
+	andlw	b'00011111'	; isolate count bits
+	decfsz	WREG,w		; subtract 1
+	goto	_nxthex
+; hex counter reached 0; print newline if needed and clear format flags
+	btfsc	LOG_FMT_FLAGS,6	; need to print a trailing newline?
+	call	_uart_print_nl
+	clrf	LOG_FMT_FLAGS
+	goto	_lsloop
+; hex counter > 0; write back new count
+_nxthex	xorwf	LOG_FMT_FLAGS,w	; xor-swap new count and old flags
+	xorwf	LOG_FMT_FLAGS,f
+	xorwf	LOG_FMT_FLAGS,w	; LOG_FMT_FLAGS contains new count, but NOT old flags
+	andlw	b'11100000'	; isolate old flags
+	iorwf	LOG_FMT_FLAGS,f	; write them back to LOG_FMT_FLAGS
+	goto	_lsloop
+
+
+
+log_single_byte
+	banksel	LOG_WREG_SAVE	; need to save W and FSR0
+	movwf	LOG_WREG_SAVE
+	movfw	FSR0L
+	movwf	LOG_FSR0L_SAVE
+	movfw	FSR0H
+	movwf	LOG_FSR0H_SAVE
+	call	_log_byte_inner	; and fall through
+log_multi_byte_end
+	banksel	LOG_FSR0L_SAVE	; (redundant when falling through)
+; restore registers
+	movfw	LOG_FSR0L_SAVE
+	movwf	FSR0L
+	movfw	LOG_FSR0H_SAVE
+	movwf	FSR0H
+	movfw	LOG_WREG_SAVE
+	return
+
+log_multi_byte_start
+; save FSR0
+	banksel	LOG_WREG_SAVE
+	movfw	FSR0L
+	movwf	LOG_FSR0L_SAVE
+	movfw	FSR0H
+	movwf	LOG_FSR0H_SAVE
+	return
+
+log_byte
+	banksel	LOG_WREG_SAVE
+	movwf	LOG_WREG_SAVE	;and fall through
+; load tail pointer into FSR0
+_log_byte_inner
+	movlw	LOG_BUFFER>>8
+	movwf	FSR0H
+	movfw	LOG_TAIL
+	movwf	FSR0L
+	movfw	LOG_WREG_SAVE	; write byte into log buffer
+	movwi	FSR0++		; advance tail pointer
+; save new tail pointer
+	movfw	FSR0L		; high byte is ignored for 256-byte wraparound
+	movwf	LOG_TAIL
+; always keep one slot open; if (tail+1)%256 == head, advance head
+	subwf	LOG_HEAD,w
+	skpnz
+	incf	LOG_HEAD,f
+	return
+
+
+_uart_print_space
+	movlw	' '
+	goto	_uart_print_ch
+_uart_print_nl
+	movlw	'\n'
+; prints char in W
+_uart_print_ch
+	banksel	TXREG
+	movwf	TXREG		; transmit the character
+	banksel	PIR1		; need 1 cycle delay before checking TXIF
+	btfss	PIR1,TXIF	; loop until character is sent
+	goto	$-1
+	banksel	LOG_FMT_FLAGS	; preserve BSR
+	return
+
+
+
+;;; Converts the lower nibble of W to its ASCII hexadecimal representation.
+_w2hd	macro			; 'w to hex digit'
+	andlw	b'00001111'
+	addlw	(256-10)	; is W >= 10?
+	skpnc
+	addlw	7		; if so, shift to letters
+	addlw	'A'-7		; shift to printable ASCII
+	endm
+_uart_print_hex
+	movfw	LOG_CURR_BYTE
+	swapf	WREG,w		; get high nibble
+	_w2hd
+	call	_uart_print_ch	; print high nibble
+	movfw	LOG_CURR_BYTE	; bring back original byte
+	_w2hd
+	goto	_uart_print_ch	; print low nibble
+
