4714c7e100a6abca0dd86a98989b01033ac65875
Matt Sarnoff <matt.sarnoff@gmail.com>
2015-02-10 10:28:33 -0800

    tweaks to sample code and gitignore

--- sample_app/blink.c
+++ sample_app/blink.c
@@ -7,26 +7,23 @@
 #include <pic14regs.h>
 #include <stdint.h>
 
-void delay(uint16_t d)
+void delay(uint32_t d)
 {
-  uint16_t i;
+  uint32_t i;
   for (i = 0; i < d; i++) {
-    __asm nop __endasm;
+    __asm
+      nop
+    __endasm;
   }
 }
 
 
 int app_main()
 {
-  // 48MHz clock from INTOSC (16MHz + 3x PLL)
-  OSCCON = _SPLLEN|_SPLLMULT|_IRCF3|_IRCF2|_IRCF1|_IRCF0;
-
   TRISAbits.TRISA5 = 0;
   while (1) {
-    PORTAbits.RA5 = 1;
-    delay(30000);
-    PORTAbits.RA5 = 0;
-    delay(30000);
+    PORTA ^= (1 << 5);
+    delay(60000);
   }
 }
 
